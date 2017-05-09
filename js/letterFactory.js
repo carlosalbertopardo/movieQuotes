5fe68ed2339cf5e60db70828850e7c136d3a0de8
carlosalbertopardo <carlos.pardo@wunderman.com>
2017-05-09 16:32:20 -0300

    factory

--- js/letterFactory.js
+++ js/letterFactory.js
@@ -34,17 +34,7 @@ LetterFactory.prototype.createLetter = function(letter) {
 		letterStructure.innerHTML = letter ;
 	}
 
-	//Generates a random id
-	/*
-	function generatId() {
-	  function s4() {
-	    return Math.floor((1 + Math.random()) * 0x10000)
-	      .toString(16)
-	      .substring(1);
-	  }
-	  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-	    s4() + '-' + s4() + s4() + s4();
-	}*/
+
 
 
 	//Bind key to letter
